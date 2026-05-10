839fad4e200b792259ea6e36cade953ec05d749d
afroz <152354779+AFROZ-CK@users.noreply.github.com>
2026-05-10 18:37:51 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'app.dart';
+import 'providers/auth_provider.dart';
+import 'providers/data_provider.dart';
+import 'providers/analytics_provider.dart';
+import 'providers/theme_provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ThemeProvider()),
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => DataProvider()),
+        ChangeNotifierProvider(create: (_) => AnalyticsProvider()),
+      ],
+      child: const DataMindApp(),
+    ),
+  );
+}
